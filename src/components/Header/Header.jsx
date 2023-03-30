9ab8c30e41fe5aca814ffdfd556816d1f7c51235
sujonmahmud102 <sujonmahmud102@gmail.com>
2023-03-30 22:46:04 +0600

    first commit

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './Header.css'
+
+const Header = () => {
+    return (
+        <div className='header'>
+            <h1>Knowledge Cafe</h1>
+            <img src="https://randomuser.me/api/portraits/men/45.jpg" alt="" />
+            
+        </div>
+    );
+};
+
+export default Header;
