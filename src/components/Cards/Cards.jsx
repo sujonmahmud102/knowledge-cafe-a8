bb535be7fc21bd1b4bd024dc83d65173b5134372
sujonmahmud102 <sujonmahmud102@gmail.com>
2023-03-31 10:32:48 +0600

    data one added

--- src/components/Cards/Cards.jsx
+++ src/components/Cards/Cards.jsx
@@ -1,34 +1,37 @@
-import React from 'react';
+
 import './Cards.css'
 import ReactDOM from 'react-dom'
-  import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-  import { faBookmark } from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faBookmark } from '@fortawesome/free-solid-svg-icons'
 
 
 
-const Cards = () => {
+const Cards = (props) => {
+    console.log(props.data)
+    const {author_img, author_name, blog_cover_img, blog_title, publish_date, read_time}=props.data;
+
     return (
         <div>
             <div className="card w-full bg-base-100 shadow-xl ">
-                <figure><img src="https://images.pexels.com/photos/276452/pexels-photo-276452.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" alt="Shoes" /></figure>
+                <figure><img src={blog_cover_img} alt="Blog Cover Image" /></figure>
                 <div className="card-body p-2">
                     <div className='author-and-bookmark flex justify-between items-center'>
                         <div className="author-info flex gap-3">
                             <div>
-                                <img src="https://pfpmaker.com/_nuxt/img/demo-thumb-2.32e1fcb.png" alt="" />
+                                <img src={author_img} alt="" />
                             </div>
                             <div>
-                                <h3 className='name text-xl font-bold'>Mr. Raju</h3>
-                                <p className='date'>Mar 14</p>
+                                <h3 className='name text-xl font-bold'>{author_name}</h3>
+                                <p className='date'>{publish_date}</p>
                             </div>
 
                         </div>
                         <div className="bookmark">
-                            <p className='bookmark-time'>05 min read <span><FontAwesomeIcon icon={faBookmark} /></span></p>
+                            <p className='bookmark-time'><span>{read_time}</span> min read <span><FontAwesomeIcon icon={faBookmark} /></span></p>
                         </div>
                     </div>
                     <div>
-                        <h2 className="card-title font-bold text-3xl my-3">How to get your first job as a self-taught programmer</h2>
+                        <h2 className="card-title font-bold text-3xl my-3">{blog_title}</h2>
                         <p className='tags'>#beginners #programming</p>
                         <button className='btn-mark-as-read'>Mark as read</button>
                     </div>
--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -1,12 +1,23 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import Cards from '../Cards/Cards';
 
 const Main = () => {
+    const [data, setData] = useState([])
+
+    useEffect(() => {
+        fetch('/public/fakeDb.json')
+            .then(res => res.json())
+            .then(data => setData(data))
+    }, [])
+
+
     return (
         <div className='main flex mx-12 gap-6 my-8'>
             <div className="card-container w-[60%]">
-                <Cards></Cards>
-            </div>  
+                {
+                    data.map(singleData => <Cards key={singleData.id} data={singleData}></Cards>)
+                }
+            </div>
             <div className="bookmark-container w-[40%] bg-red-600">
                 <h2>bookmark</h2>
             </div>
