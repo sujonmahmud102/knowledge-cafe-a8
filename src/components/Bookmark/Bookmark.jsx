065c68619700688dcdc581f118c71bf4d929f605
sujonmahmud102 <sujonmahmud102@gmail.com>
2023-03-31 11:52:49 +0600

    bookmark component design done

--- src/components/Bookmark/Bookmark.jsx
+++ src/components/Bookmark/Bookmark.jsx
@@ -3,9 +3,10 @@ import './Bookmark.css'
 
 const Bookmark = () => {
     return (
-        <div>
+        <div className='bookmark'>
             <h2>Bookmarked Blogs : </h2>
-            
+
+            <p className="blog-title">Master Microsoft Power Platform and Become an In-Demand!</p>
         </div>
     );
 };
