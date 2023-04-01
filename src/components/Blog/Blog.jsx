7384d29da3fa65fca586d07f8a627991311ee6db
sujonmahmud102 <sujonmahmud102@gmail.com>
2023-04-01 11:18:35 +0600

    toast added

--- src/components/Blog/Blog.jsx
+++ src/components/Blog/Blog.jsx
@@ -5,8 +5,8 @@ const Blog = () => {
     return (
         <>
             <div>
-                <h2>Q1: Props vs state</h2>
-                <p>Props are short for "properties," and they are used to pass data from a parent component to a child component. Props are read-only, meaning that a child component cannot modify the values of its props. Instead, the parent component must modify its own state and pass the new data down to the child component via props. Props are useful for creating reusable and modular components, as well as for passing data and behavior down the component tree.
+                <h2 className='text-xl font-semibold my-3'>Q1: Props vs state</h2>
+                <p className='text-justify'>Props are short for "properties," and they are used to pass data from a parent component to a child component. Props are read-only, meaning that a child component cannot modify the values of its props. Instead, the parent component must modify its own state and pass the new data down to the child component via props. Props are useful for creating reusable and modular components, as well as for passing data and behavior down the component tree.
                     <br />
                     <br />
 
@@ -14,24 +14,24 @@ const Blog = () => {
                 </p>
             </div>
             <div>
-                <h2>Q2: How does useState work?
+                <h2 className='text-xl font-semibold my-3'>Q2: How does useState work?
                 </h2>
-                <p>useState is React Hook that allows you to add state to a functional component. It returns an array with two values: the current state and a function to update it. The Hook takes an initial state value as an argument and returns an updated state value whenever the setter function is called.
+                <p className='text-justify'>useState is React Hook that allows you to add state to a functional component. It returns an array with two values: the current state and a function to update it. The Hook takes an initial state value as an argument and returns an updated state value whenever the setter function is called.
 
                 </p>
             </div>
             <div>
-                <h2>Q3: Purpose of useEffect other than fetching data.
+                <h2 className='text-xl font-semibold my-3'>Q3: Purpose of useEffect other than fetching data.
                 </h2>
-                <p>
+                <p className='text-justify'>
                     The useEffect in react js allows you to perform side effects in your components. The react useEffect examples of side effects include retrieving data, direct DOM updates, and timers. The second argument is optional.
                 </p>
             </div>
             <div>
-                <h2>Q4: How Does React work?
+                <h2 className='text-xl font-semibold my-3'>Q4: How Does React work?
 
                 </h2>
-                <p>
+                <p className='text-justify mb-3'>
                     ReactJS divides the UI into isolated reusable pieces of code known as components. React components work similarly to JavaScript functions as they accept arbitrary inputs called properties or props. It's possible to have as many components as necessary without cluttering your code.
                 </p>
             </div>
--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -3,6 +3,8 @@ import Cards from '../Cards/Cards';
 import './Main.css'
 import Bookmark from '../Bookmark/Bookmark';
 import Blog from '../Blog/Blog';
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 const Main = () => {
     const [data, setData] = useState([])
@@ -16,12 +18,22 @@ const Main = () => {
     }, [])
 
     const handleBookmark = (data) => {
+
         const newBlog = [...blog, data];
         setBlog(newBlog);
+    
+        const blogId = blog.find(singleBlog =>{
+            console.log(singleBlog)
+            if(singleBlog.id === data.id){
+                toast.error ('You Have Already Bookmarked This Blog')
+            }
+        })
     }
 
+
+
+
     const markRead = (data) => {
-        console.log(data.read_time)
         const newReadTime = [...readTime, data.read_time]
         setReadTime(newReadTime);
     }
@@ -38,12 +50,14 @@ const Main = () => {
                     data.map(singleData => <Cards markRead={markRead} handleBookmark={handleBookmark} key={singleData.id} data={singleData}></Cards>)
                 }
                 <Blog></Blog>
+                <ToastContainer />
             </div>
             <div className="bookmark-container lg:w-[40%]">
                 <div className='spent-time-and-bookmark'>
-                    <h2 className='spent-time'>Spent time on read : {sum} min</h2>
-                    <Bookmark blog={blog}></Bookmark>
+                    <h2 className='spent-time'>Spent time on read : {sum} mins</h2>
+                    <Bookmark key={blog.id} blog={blog}></Bookmark>
                 </div>
+                
             </div>
         </div>
     );
